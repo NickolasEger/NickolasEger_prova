ed9070dc43dbea29cc7979d0d63402959b1af13b
NickolasEger <nickolas_eger@estudante.sesisenai.org.br>
2025-08-11 14:39:59 -0300

    commit

--- MaterialApoio/principal.php
+++ MaterialApoio/principal.php
@@ -19,52 +19,76 @@ $nome_perfil = $perfil['nome_perfil'];
 //DEFINIÇÃO DAS PERMISSOES POR PERFIL
 
 $permissoes = [
-    1 =>["Cadastrar"=>["cadastro_usuario.php","cadastro_perfil.php","cadastro_cliente.php","cadastro_fornecedor.php","cadastro_produto.php","cadastro_funcionario.php"],
-    "Buscar"=>["buscar_usuario.php","buscar_perfil.php","buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php","buscar_funcionario.php"],
-    "Alterar"=>["alterar_usuario.php","alterar_perfil.php","alterar_cliente.php","alterar_fornecedor.php","alterar_produto.php","alterar_funcionario.php"],
-    "Excluir"=>["excluir_usuario.php","excluir_perfil.php","excluir_cliente.php","excluir_fornecedor.php","excluir_produto.php","excluir_funcionario.php"]],
+    1 => [
+        "Cadastrar" => ["cadastro_usuario.php","cadastro_perfil.php","cadastro_cliente.php","cadastro_fornecedor.php","cadastro_produto.php","cadastro_funcionario.php"],
+        "Buscar"    => ["buscar_usuario.php","buscar_perfil.php","buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php","buscar_funcionario.php"],
+        "Alterar"   => ["alterar_usuario.php","alterar_perfil.php","alterar_cliente.php","alterar_fornecedor.php","alterar_produto.php","alterar_funcionario.php"],
+        "Excluir"   => ["excluir_usuario.php","excluir_perfil.php","excluir_cliente.php","excluir_fornecedor.php","excluir_produto.php","excluir_funcionario.php"]
+    ],
 
-    2 =>["Cadastrar"=>["cadastro_cliente.php"],
-    "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
-    "Alterar"=>["alterar_fornecedor.php","alterar_produto.php"],
-    "Excluir"=>["excluir_produto.php"]],
+    2 => [
+        "Cadastrar" => ["cadastro_cliente.php"],
+        "Buscar"    => ["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
+        "Alterar"   => ["alterar_fornecedor.php","alterar_produto.php"],
+        "Excluir"   => ["excluir_produto.php"]
+    ],
 
-    3 =>["Cadastrar"=>["cadastro_fornecedor.php","cadastro_produto"],
-    "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
-    "Alterar"=>["alterar_fornecedor.php","alterar_produto.php"],
-    "Excluir"=>["excluir_produto.php"]],
+    3 => [
+        "Cadastrar" => ["cadastro_fornecedor.php","cadastro_produto.php"],
+        "Buscar"    => ["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
+        "Alterar"   => ["alterar_fornecedor.php","alterar_produto.php"],
+        "Excluir"   => ["excluir_produto.php"]
+    ],
 
-    4 =>["Cadastrar"=>["cadastro_cliente.php"],
-    "Buscar"=>["buscar_produto.php"],
-    "Alterar"=>["alterar_cliente.php"],
+    4 => [
+        "Cadastrar" => ["cadastro_cliente.php"],
+        "Buscar"    => ["buscar_produto.php"],
+        "Alterar"   => ["alterar_produto.php"]
     ]
 ];
 
+// OBTENDO AS OPÇÕES DISPONÍVEIS PARA O PERFIL LOGADO
+$id_perfil = $_SESSION['id_perfil']; // ou qualquer variável que contenha o ID do perfil
+$opcoes_menu = $permissoes[$id_perfil];
 
-// obtendo as opcoes disponiveis para o perfil logado
-$opcoes_menu = $permissoes['id_perfil'];
+
 ?>
-
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Painel principal</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Principal</title>
+    <link rel = "stylesheet" href = "styles.css">
     <script src="scripts.js"></script>
 </head>
 <body>
     <header>
-        <div>
-            <h2>Bem vindo, <?php echo $_SESSION["usuario"]; ?>! Perfil: <?php echo $nome_perfil;?></h2>
+        <div class="saudacao">
+            <h2>Bem Vindo, <?php echo $_SESSION["usuario"];?> ! Perfil: <?php echo $nome_perfil;?></h2>
         </div>
 
         <div class="logout">
             <form action="logout.php" method="POST">
-                <button type="submit">Logout</button>
+                <button type="submit"></button>
             </form>
         </div>
     </header>
+    <nav>
+        <ul class="menu">
+            <?php foreeach($opcoes_menu as $categoria =>$arquivos);?>
+            <li class="dropdown">
+                <a href="#"><?=$categoria?></a>
+                <ul class="dropdown-menu">
+                    <?php foreeach($arquivos as $arquivo);?>
+                    <li>
+                        <a href="<? $arquivo?>"<?=ucfirst(str_replace("_","",basename($arquivos,".php")))?>></a>
+                    </li>
+                    <?php foreach;?>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+
 </body>
 </html>
