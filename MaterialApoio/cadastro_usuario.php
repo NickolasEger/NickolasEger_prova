b6f6362d9f53f16257889bfce0e42d5ebe32e12e
NickolasEger <nickolas_eger@estudante.sesisenai.org.br>
2025-08-11 16:39:39 -0300

    commit

--- MaterialApoio/cadastro_usuario.php
+++ MaterialApoio/cadastro_usuario.php
@@ -1,21 +1,21 @@
-<?php 
+<?php
 session_start();
 require_once 'conexao.php';
 
-//VERIFICA SE O USUARIO TEM PERMISSAO
-//SUPONDO QUE O PERFIL 1 SEJA O ADMINISTRADOR   
+//verifica se o usuario tem permissao
+//supondo que o perfil 1 seja o admin
 
 if($_SESSION['perfil']!=1){
     echo "Acesso Negado!";
 }
 
-if($_SERVER["REQUEST_METHOD"]=="POST"){
+if ($_SERVER["REQUEST_METHOD"]=="POST") {
     $nome = $_POST['nome'];
     $email = $_POST['email'];
-    $senha = password_hash($_POST['senha'],PASSWORD_DEFAULT);
+    $senha = password_hash ($_POST['senha'],PASSWORD_DEFAULT);
     $id_perfil = $_POST['id_perfil'];
 
-    $sql="INSERT INTO usuario(nome,email,senha,id_perfil) VALUES (:nome,:email,:senha,:id_perfil)";
+    $sql="INSERT INTO usuario(nome,email,senha,id_perfil) VALUES (:nome. :email, :senha, :id_perfil)";
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':nome',$nome);
     $stmt->bindParam(':email',$email);
@@ -23,41 +23,40 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     $stmt->bindParam(':id_perfil',$id_perfil);
 
     if($stmt->execute()){
-        echo "<script>alert('Usuario cadastrado com sucesso!');</script>";
-    }else{
-        echo "<script>alert('Erro ao cadastrar o usuario!');</script>";
+        echo "<script>alert('Usuario cadastrado com sucesso!';</script>";
+    } else{
+        echo "<script>alert('Erro ao cadastrado usuario';</script>";
     }
 }
 ?>
-
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastrar Usuario</title>
+    <title>Painel Principal</title>
     <link rel = "stylesheet" href = "styles.css">
 </head>
 <body>
     <h2>Cadastrar Usuario</h2>
     <form action="cadastro_usuario.php" method="POST">
-
         <label for="nome">Nome:</label>
         <input type="text" id="nome" name="nome" required>
-        
-        <label for="email">email:</label>
+
+        <label for="email">Email:</label>
         <input type="email" id="email" name="email" required>
 
-        <label for="senha">senha:</label>
+        <label for="senha">Senha:</label>
         <input type="password" id="senha" name="senha" required>
 
-        <label for="id_perfil">Perfil:</label>
+        <label for="id_perfil">Perfil</label>
         <select id="id_perfil" name="id_perfil">
             <option value="1">Administrador</option>
             <option value="2">Secretaria</option>
             <option value="3">Almoxarife</option>
             <option value="4">Cliente</option>
         </select>
+
         <button type="submit">Salvar</button>
         <button type="reset">Cancelar</button>
     </form>
